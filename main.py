cd139ff1262e227778167df24e595247e470b914
Nathan Van Maastricht <nathan.vanmaastricht@priorianalytica.com>
2024-11-10 20:28:46 +1030

    Feat: Adjusted main.py to call training function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from environment.game import Game
 from utils.config import Config
+from utils.brain_trainer import BrainTrainer
+from agents.brain import Brain
+from agents.brain import BrainValue
 
 
 def main():
@@ -8,5 +11,17 @@ def main():
     game.run()
 
 
+def train():
+    brain = Brain(16, 128)
+    critic = BrainValue(16, 128)
+    config = Config("config.json")
+    trainer = BrainTrainer(brain, critic, config)
+    for epoch in range(20):
+        print(f"Starting epoch {epoch}")
+        trainer.train_episode()
+        # brain.save(f"weights/{epoch}.pth")
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    train()
