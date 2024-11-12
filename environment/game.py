9429416995721018d43d1402e1b6ecc818938e57
Nathan Van Maastricht <nathan.vanmaastricht@gmail.com>
2024-11-12 10:52:11 +1030

    Feat: Randomly offset the puck at the beginning of the game to introduce some variation in reward

--- environment/game.py
+++ environment/game.py
@@ -145,8 +145,8 @@ class Game:
         self.agents = []
         self.create_agents()
         self.puck = Puck(
-            self.config["field"]["width"] / 2,
-            self.config["field"]["height"] / 2,
+            self.config["field"]["width"] / 2 + random.gauss(0, 100),
+            self.config["field"]["height"] / 2 + random.gauss(0, 30),
             self.config,
         )
 
