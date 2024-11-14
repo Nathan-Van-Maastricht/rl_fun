cb8431939b99eefa577a24a514a6020728df4633
Nathan Van Maastricht <nathan.vanmaastricht@gmail.com>
2024-11-14 13:21:52 +1030

    Feat: Adding saving and loading of critic state

--- main.py
+++ main.py
@@ -13,15 +13,19 @@ def main():
 def train():
     brain = Brain(456, 128)
     critic = BrainValue(456, 128)
-    # brain.load("weights/0105.pth")
+    load_batch = 0
+    if load_batch > 0:
+        brain.load(f"weights/b{load_batch:0>5}.pth")
+        critic.load(f"weights/c{load_batch:0>5}.pth")
     config = Config("config.json")
     trainer = BrainTrainer(brain, config, critic)
-    for epoch in range(0, 9996):
+    for epoch in range(load_batch + 1, 3001):
         print(f"Starting epoch {epoch}")
         trainer.train_episode()
         trainer.decay_epsilon()
         if epoch % 5 == 0 and config["learn"]:
-            brain.save(f"weights/{epoch:0>4}.pth")
+            brain.save(f"weights/b{epoch:0>5}.pth")
+            critic.save(f"weights/c{epoch:0>5}.pth")
 
 
 if __name__ == "__main__":
