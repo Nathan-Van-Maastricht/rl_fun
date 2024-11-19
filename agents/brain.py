3e500639671bd974be313c112ab349d5200c7102
Nathan Van Maastricht <nathan.vanmaastricht@gmail.com>
2024-11-19 11:54:01 +1030

    Feat: Added Swish activation layers, and a different embedding for team mates vs enemies

--- agents/brain.py
+++ agents/brain.py
@@ -22,16 +22,25 @@ class Embedding(nn.Module):
         return torch.matmul(input.float().unsqueeze(0), self.embedding).squeeze(0)
 
 
+class Swish(nn.Module):
+    def __init__(self, beta=1.0):
+        super(Swish, self).__init__()
+        self.beta = nn.Parameter(torch.tensor(beta))
+
+    def forward(self, x):
+        return x * torch.sigmoid(self.beta * x)
+
+
 class Brain(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(Brain, self).__init__()
         self.accelerate = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             # nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
+            nn.Linear(hidden_dim, 2 * hidden_dim),
+            Swish(beta=1.0),
+            nn.Linear(2 * hidden_dim, hidden_dim),
+            Swish(beta=1.0),
             nn.Linear(hidden_dim, 2),
             nn.Softmax(),
         )
@@ -39,39 +48,70 @@ class Brain(nn.Module):
         self.turn = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             # nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, 15),
+            nn.Linear(hidden_dim, 2 * hidden_dim),
+            Swish(beta=1.0),
+            nn.Linear(2 * hidden_dim, hidden_dim),
+            Swish(beta=1.0),
+            nn.Linear(hidden_dim, 5),
             nn.Softmax(),
         )
 
-        self.embedding = Embedding(2, 64)
+        self.team_mate_embedding = Embedding(3, 32)
+        self.enemy_embedding = Embedding(3, 32)
 
     def forward(self, status, distances, positions):
-        puck_pos = self.embedding(positions[0:2])
-        self_pos = self.embedding(positions[2:4])
-        team_1_pos = self.embedding(positions[4:6])
-        team_2_pos = self.embedding(positions[6:8])
-        enemy_1_pos = self.embedding(positions[8:10])
-        enemy_2_pos = self.embedding(positions[10:12])
-        enemy_3_pos = self.embedding(positions[12:14])
+        # puck_pos = self.embedding(positions[0:2])
+        # self_pos = self.embedding(positions[2:4])
+        # team_1_pos = self.embedding(positions[4:6])
+        # team_2_pos = self.embedding(positions[6:8])
+        # enemy_1_pos = self.embedding(positions[8:10])
+        # enemy_2_pos = self.embedding(positions[10:12])
+        # enemy_3_pos = self.embedding(positions[12:14])
 
-        if torch.any(puck_pos == 0):
-            print(f"{puck_pos=}")
+        # if torch.any(puck_pos == 0):
+        #     print(f"{puck_pos=}")
+
+        # vector = torch.cat(
+        #     (
+        #         status,
+        #         distances,
+        #         puck_pos,
+        #         self_pos,
+        #         team_1_pos,
+        #         team_2_pos,
+        #         enemy_1_pos,
+        #         enemy_2_pos,
+        #         enemy_3_pos,
+        #     )
+        # )
+        puck_pos = torch.FloatTensor(positions[0:2])
+        self_pos = torch.FloatTensor(positions[2:4])
+        team_1_embedding = self.team_mate_embedding(
+            torch.cat((positions[4:6], distances[0].unsqueeze(0)))
+        )
+        team_2_embedding = self.team_mate_embedding(
+            torch.cat((positions[6:8], distances[1].unsqueeze(0)))
+        )
+        enemy_1_embedding = self.enemy_embedding(
+            torch.cat((positions[8:10], distances[2].unsqueeze(0)))
+        )
+        enemy_2_embedding = self.enemy_embedding(
+            torch.cat((positions[10:12], distances[3].unsqueeze(0)))
+        )
+        enemy_3_embedding = self.enemy_embedding(
+            torch.cat((positions[12:14], distances[4].unsqueeze(0)))
+        )
 
         vector = torch.cat(
             (
                 status,
-                distances,
                 puck_pos,
                 self_pos,
-                team_1_pos,
-                team_2_pos,
-                enemy_1_pos,
-                enemy_2_pos,
-                enemy_3_pos,
+                team_1_embedding,
+                team_2_embedding,
+                enemy_1_embedding,
+                enemy_2_embedding,
+                enemy_3_embedding,
             )
         )
 
@@ -96,37 +136,72 @@ class BrainValue(nn.Module):
             nn.Linear(input_dim, hidden_dim),
             # nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
+            Swish(beta=1.0),
             nn.Linear(hidden_dim, 1),
         )
 
-        self.embedding = Embedding(2, 64)
+        self.team_mate_embedding = Embedding(3, 32)
+        self.enemy_embedding = Embedding(3, 32)
 
     def forward(self, status, distances, positions):
-        puck_pos = self.embedding(positions[0:2])
-        self_pos = self.embedding(positions[2:4])
-        team_1_pos = self.embedding(positions[4:6])
-        team_2_pos = self.embedding(positions[6:8])
-        enemy_1_pos = self.embedding(positions[8:10])
-        enemy_2_pos = self.embedding(positions[10:12])
-        enemy_3_pos = self.embedding(positions[12:14])
+        # puck_pos = self.embedding(positions[0:2])
+        # self_pos = self.embedding(positions[2:4])
+        # team_1_pos = self.embedding(positions[4:6])
+        # team_2_pos = self.embedding(positions[6:8])
+        # enemy_1_pos = self.embedding(positions[8:10])
+        # enemy_2_pos = self.embedding(positions[10:12])
+        # enemy_3_pos = self.embedding(positions[12:14])
 
-        if torch.any(puck_pos == 0):
-            print(f"{puck_pos=}")
+        # if torch.any(puck_pos == 0):
+        #     print(f"{puck_pos=}")
+
+        # vector = torch.cat(
+        #     (
+        #         status,
+        #         distances,
+        #         puck_pos,
+        #         self_pos,
+        #         team_1_pos,
+        #         team_2_pos,
+        #         enemy_1_pos,
+        #         enemy_2_pos,
+        #         enemy_3_pos,
+        #     )
+        # )
+
+        puck_pos = torch.FloatTensor(positions[0:2])
+        self_pos = torch.FloatTensor(positions[2:4])
+        team_1_embedding = self.team_mate_embedding(
+            torch.cat((positions[4:6], distances[0].unsqueeze(0)))
+        )
+        team_2_embedding = self.team_mate_embedding(
+            torch.cat((positions[6:8], distances[1].unsqueeze(0)))
+        )
+        enemy_1_embedding = self.enemy_embedding(
+            torch.cat((positions[8:10], distances[2].unsqueeze(0)))
+        )
+        enemy_2_embedding = self.enemy_embedding(
+            torch.cat((positions[10:12], distances[3].unsqueeze(0)))
+        )
+        enemy_3_embedding = self.enemy_embedding(
+            torch.cat((positions[12:14], distances[4].unsqueeze(0)))
+        )
+
+        # vector = torch.cat((status, distances, positions))
 
         vector = torch.cat(
             (
                 status,
-                distances,
                 puck_pos,
                 self_pos,
-                team_1_pos,
-                team_2_pos,
-                enemy_1_pos,
-                enemy_2_pos,
-                enemy_3_pos,
+                team_1_embedding,
+                team_2_embedding,
+                enemy_1_embedding,
+                enemy_2_embedding,
+                enemy_3_embedding,
             )
         )
+
         return self.main(vector)
 
     def save(self, path):
